2e12df7142dbefa9135edb579661ca3d0845687b
Matty <54020396+matty-ross@users.noreply.github.com>
2023-12-03 19:59:36 +0100

    Refactored loader + mods' descriptions.

--- loader.js
+++ loader.js
@@ -1,38 +1,56 @@
-async function getMods() {
-    const response = await fetch('/mods.json');
-    const mods = await response.json();
-    return mods;
-}
-
-async function getModsPage() {
-    const response = await fetch('/pages/mods.html');
-    const modsPage = await response.text();
-    return modsPage;
-}
-
-function createModCard(mod) {
-    const modCard = document.querySelector('#mod-card').content.cloneNode(true);
-    modCard.querySelector('a').href = `?mod=${mod.id}`;
-    modCard.querySelector('.card-title').textContent = mod.title;
-    modCard.querySelector('.card-text').textContent = mod.description;
-    return modCard;
-}
-
-
-(async function () {
-    const mods = await getMods();
-    const main = document.querySelector('main');
-    const queryMod = new URLSearchParams(location.search).get('mod');
-    
-    const mod = mods.find(mod => mod.id === queryMod);
-    if (mod === undefined) {
-        const modsPage = await getModsPage();
-        main.innerHTML = modsPage;
-        for (const mod of mods) {
-            const modCard = createModCard(mod);
-            document.querySelector('#mods').append(modCard);
-        }
-    } else {
-        console.log('unimplemented yet');
+class Loader {
+    constructor() {
+        this.mods = null;
+        this.main = document.querySelector('main');
+        this.queryMod = new URLSearchParams(location.search).get('mod');
     }
+
+    async load() {
+        this.mods = await Loader.#getMods();
+        const mod = this.mods.find(mod => mod.id === this.queryMod);
+        if (mod === undefined) {
+            this.main.innerHTML = await this.#getModsPage();
+        } else {
+            this.main.innerHTML = await this.#getModPage(mod);
+        }
+    }
+
+    async #getModsPage() {
+        const dummyDiv = document.createElement('div');
+        dummyDiv.innerHTML = await Loader.#getPage('mods');
+        for (const mod of this.mods) {
+            const modCard = dummyDiv.querySelector('#mod-card').content.cloneNode(true);
+            modCard.querySelector('a').href = `?mod=${mod.id}`;
+            modCard.querySelector('.card-title').textContent = mod.title;
+            modCard.querySelector('.card-text').textContent = mod.description;
+            dummyDiv.querySelector('ul').append(modCard);
+        }
+        return dummyDiv.innerHTML;
+    }
+
+    async #getModPage(mod) {
+        const dummyDiv = document.createElement('div');
+        dummyDiv.innerHTML = await Loader.#getPage('mod');
+        dummyDiv.querySelector('.mod-title').textContent = mod.title;
+        dummyDiv.querySelector('.mod-description').textContent = mod.description;
+        return dummyDiv.innerHTML;
+    }
+
+    static async #getMods() {
+        const response = await fetch('/mods.json');
+        const json = await response.json();
+        return json;
+    }
+
+    static async #getPage(page) {
+        const response = await fetch(`/pages/${page}.html`);
+        const html = await response.text();
+        return html;
+    }
+}
+
+
+(function () {
+    const loader = new Loader();
+    loader.load();
 })();
