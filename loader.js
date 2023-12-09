7ed5e2def25af107e79669f857ca074e649742d4
Matty <54020396+matty-ross@users.noreply.github.com>
2023-12-09 15:01:01 +0100

    Added mod notes.

--- loader.js
+++ loader.js
@@ -3,21 +3,39 @@ const mods = [
         id: 'exception-reporter',
         title: 'Exception Reporter',
         description: "Displays a dialog with exception's information when it occurs.",
+        notes: [
+            "It may not catch every exception.",
+            "Don't expect people immediatelly knowing what's wrong by uploading a screenshot of the exception report.",
+        ],
     },
     {
         id: 'free-camera',
         title: 'Free Camera',
         description: "Allows moving the external camera around the city. Plus various camera properties that can be changed.",
+        notes: [
+            "Press F7 to toggle the menu.",
+            "Use your mouse to freely transform the camera.",
+        ],
     },
     {
         id: 'bully-repellent',
         title: 'Bully Repellent',
         description: "Automatically kicks or mutes a player on the user's blacklist.",
+        notes: [
+            "Press F7 to toggle the menu.",
+            "Players added to the blacklist aren't automatically kicked or muted, you must check either 'Autokick' or 'Automute'.",
+            "You cannot add yourself to the list, nor the same player multiple times.",
+        ],
     },
     {
         id: 'protection',
         title: 'Protection',
-        description: "Replaces the new vehicle's or challenge's ID sent over the network to avoid crashing players.",
+        description: "Replaces a new vehicle's or challenge's ID sent over the network to avoid crashing players.",
+        notes: [
+            "Press F7 to toggle the menu.",
+            "New vehicles and challenges are added to the protection list automatically.",
+            "You can individually set the replacement vehicle/challenge, or let it use the fallback vehicle/challenge.",
+        ],
     },
 ];
 
@@ -55,6 +73,11 @@ class Loader {
         dummyDiv.innerHTML = await Loader.#getPage('mod');
         dummyDiv.querySelector('.mod-title').textContent = mod.title;
         dummyDiv.querySelector('.mod-description').textContent = mod.description;
+        for (const note of mod.notes) {
+            const li = document.createElement('li');
+            li.textContent = note;
+            dummyDiv.querySelector('.mod-notes').append(li);
+        }
         return dummyDiv.innerHTML;
     }
 
